a63478c79a888f2a80785a6f693783f573efce1d
Thiago Ritcher <thiagoritcher@gmail.com>
2024-02-04 19:37:39 -0300

    Complete vim config and Vimplug install script

--- user.install.sh
+++ user.install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+#ohmyzsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+#vimplug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 ln -s $PWD/dotzshrc $HOME/.zshrc
 ln -s $PWD/dotvimrc $HOME/.vimrc
 ln -s $PWD/dottmux.conf $HOME/.tmux.conf
